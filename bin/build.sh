0d5e0a3642f1a90fa43702d6f83caa4ce022a01e
Chris Pitt <chris@72days.dev>
2025-11-05 14:53:55 +0000

    chore: copy countries file

--- bin/build.sh
+++ bin/build.sh
@@ -3,5 +3,6 @@
 rm -rf ./dist && \
 mkdir dist && mkdir dist/resources && mkdir dist/resources/flags &&  \
 cp -r ./src/resources/flags/images ./dist/resources/flags && \
+cp ./src/resources/countries.json ./dist/resources/ && \
 mkdir dist/typings && cp -r ./src/typings ./dist \
 && tsc
